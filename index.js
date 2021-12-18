ed45adb146d8ecf579d2f047223d6d17ab7888c5
clove10002 <82989516+clove10002@users.noreply.github.com>
2021-12-18 20:04:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const app = express();
 
-const hostname = '0.0.0.0';
-const port = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
   readFile('./index.html' 'utf8', (err, html) => {
-    if(err){
-      res.status(500).send("error 500!");
-    }
-    res.send(html);    
+    res.send(html);
   })
   
 });

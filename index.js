189ffe04e9ff37e6234c1e990ea23e8d4437ac55
clove10002 <82989516+clove10002@users.noreply.github.com>
2021-12-18 20:26:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  /*readFile('./index.html', 'utf8', (err, html) => {
-    res.send(html);
-  });*/
-  res.send("helos");
-});
+app.get('/', (req, res) => res.send("helos"));
 
-app.listen(process.env.PORT || 3000, () => console.log('App available'))
+app.listen(process.env.PORT || 3000, () => console.log('App available'));

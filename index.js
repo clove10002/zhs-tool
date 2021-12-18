779d2820326ea3601386f6883d7076f3256c6453
clove10002 <82989516+clove10002@users.noreply.github.com>
2021-12-18 19:55:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,15 @@ const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  readFile('./index.html' 'utf8', (err, html) => {
+    if(err){
+      res.status(500).send("error 500!");
+    }
+    res.send(html);    
+  })
+  
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-*/
-app.get('/', (request, response) => {
-  readFile('index.html', 'utf8', (err, html)) = {
-    if(err){
-      response.status(500).send("error 500!");
-    }
-    response.send(html);
-  }
-});
-
-app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
